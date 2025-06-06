9cd533fa805a7850d671437fd5a19bda4dff6162
Krishna <ks3259265@gmail.com>
2025-06-06 14:50:33 +0530

    everything is updated

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,11 @@
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/student/login', methods=['GET', 'POST'])
+def student_login():
+    if request.method == 'POST':
+        username = request.form['username'].strip()
+        password = request.form['password'].strip()
+        students = read_accounts(STUDENTS_FILE)
+        if username in students and students[username] == password:
+            session['student'] = username
+            return redirect(url_for('student_panel'))
+        return "Invalid student credentials! <a href='/student/login'>Try Again</a>"
+    return render_template('student_login.html')
